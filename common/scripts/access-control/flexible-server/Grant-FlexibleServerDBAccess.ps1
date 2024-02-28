2d7ae5d5d3b28afdc0851815d3a469f723cabcb9
Jeevan Kuduva Ravindran <119388966+jeevankuduvaravindran@users.noreply.github.com>
2024-02-28 17:43:00 +0000

    Revert "307513: Add managed ID to postgres AD group on pre-migration stage (#57)"
    This reverts commit 5056d7bee73957700f6a990024bf2c2691d1d79c.

--- common/scripts/access-control/flexible-server/Grant-FlexibleServerDBAccess.ps1
+++ common/scripts/access-control/flexible-server/Grant-FlexibleServerDBAccess.ps1
@@ -14,14 +14,13 @@ Set-StrictMode -Version 3.0
 [string]$PostgresHost = $env:POSTGRES_HOST
 [string]$PostgresDatabase = $env:POSTGRES_DATABASE
 [string]$ServiceMIName = $env:SERVICE_MI_NAME
+[string]$TeamMIName = $env:TEAM_MI_NAME
 [string]$TeamMIClientId = $env:AZURE_CLIENT_ID
 [string]$TeamMITenantId = $env:AZURE_TENANT_ID
 [string]$TeamMISubscriptionId = $env:TEAM_MI_SUBSCRIPTION_ID
 [string]$TeamMIFederatedTokenFile = $env:AZURE_FEDERATED_TOKEN_FILE
 [string]$SubscriptionName = $env:SUBSCRIPTION_NAME
 [string]$WorkingDirectory = $PWD
-[string]$PostgresReaderAdGroup = $env:PG_READER_AD_GROUP
-[string]$PostgresWriterAdGroup = $env:PG_WRITER_AD_GROUP
 
 [string]$functionName = $MyInvocation.MyCommand
 [DateTime]$startTime = [DateTime]::UtcNow
@@ -41,37 +40,21 @@ Write-Host "${functionName} started at $($startTime.ToString('u'))"
 Write-Debug "${functionName}:PostgresHost:$PostgresHost"
 Write-Debug "${functionName}:PostgresDatabase:$PostgresDatabase"
 Write-Debug "${functionName}:ServiceMIName:$ServiceMIName"
+Write-Debug "${functionName}:TeamMIName:$TeamMIName"
 Write-Debug "${functionName}:SubscriptionName=$SubscriptionName"
 Write-Debug "${functionName}:WorkingDirectory=$WorkingDirectory"
-Write-Debug "${functionName}:PostgresReaderAdGroup=$PostgresReaderAdGroup"
-Write-Debug "${functionName}:PostgresWriterAdGroup=$PostgresWriterAdGroup"
 
 [System.IO.DirectoryInfo]$scriptDir = $PSCommandPath | Split-Path -Parent
 Write-Debug "${functionName}:scriptDir.FullName:$($scriptDir.FullName)"
 
-function Get-SQLScriptToGrantReadPermissions {
-    [System.Text.StringBuilder]$builder = [System.Text.StringBuilder]::new()
-    [void]$builder.Append("GRANT USAGE ON SCHEMA public TO `"$PostgresReaderAdGroup`";")
-    [void]$builder.Append("GRANT SELECT ON ALL TABLES IN SCHEMA public TO `"$PostgresReaderAdGroup`";")
-    [void]$builder.Append("GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO `"$PostgresReaderAdGroup`";")
-    [void]$builder.Append("REVOKE EXECUTE ON ALL FUNCTIONS IN SCHEMA public FROM `"$PostgresReaderAdGroup`";")
-    [void]$builder.Append("REVOKE EXECUTE ON ALL PROCEDURES IN SCHEMA public FROM `"$PostgresReaderAdGroup`";")
-    [void]$builder.Append("ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO `"$PostgresReaderAdGroup`";")
-    [void]$builder.Append("ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON SEQUENCES TO `"$PostgresReaderAdGroup`";")
-    [void]$builder.Append("ALTER DEFAULT PRIVILEGES IN SCHEMA public REVOKE EXECUTE ON FUNCTIONS FROM `"$PostgresReaderAdGroup`";")
-    return $builder.ToString()
-}
-
-function Get-SQLScriptToGrantServicePermissions {
+function Get-SQLScriptToGrantPermissions {
     [System.Text.StringBuilder]$builder = [System.Text.StringBuilder]::new()
     [void]$builder.Append("GRANT CREATE, USAGE ON SCHEMA public TO `"$ServiceMIName`";")
     [void]$builder.Append("GRANT SELECT, UPDATE, INSERT, REFERENCES, TRIGGER ON ALL TABLES IN SCHEMA public TO `"$ServiceMIName`";")
     [void]$builder.Append("GRANT SELECT, UPDATE, USAGE ON ALL SEQUENCES IN SCHEMA public TO `"$ServiceMIName`";")
     [void]$builder.Append("GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA public TO `"$ServiceMIName`";")
     [void]$builder.Append("GRANT EXECUTE ON ALL PROCEDURES IN SCHEMA public TO `"$ServiceMIName`";")
-    [void]$builder.Append("ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, UPDATE, INSERT, REFERENCES, TRIGGER ON TABLES TO `"$ServiceMIName`";")
-    [void]$builder.Append("ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, USAGE ON SEQUENCES TO `"$ServiceMIName`";")
-    [void]$builder.Append("ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT EXECUTE ON FUNCTIONS TO `"$ServiceMIName`";")
+    
     return $builder.ToString()
 }
 
@@ -90,7 +73,7 @@ try {
     $ENV:PGPASSWORD = $accessToken.Token
     Write-Host "Access Token Acquired"
 
-    [string]$command = Get-SQLScriptToGrantServicePermissions
+    [string]$command = Get-SQLScriptToGrantPermissions
     Write-Debug "${functionName}:command=$command"
     
     [System.IO.FileInfo]$assignPermissionsTempFile = [System.IO.Path]::GetTempFileName()
@@ -98,18 +81,7 @@ try {
     Write-Debug "${functionName}:$($assignPermissionsTempFile.FullName)=$content"
     
     Write-Host "Granting permissions to ${ServiceMIName}"
-    $null = Invoke-PSQLScript -PostgresHost $PostgresHost -PostgresDatabase $PostgresDatabase -PostgresUsername $PostgresWriterAdGroup -Path $assignPermissionsTempFile.FullName
-    Write-Host "Granted Access to ${PostgresHost}"
-
-    [string]$command = Get-SQLScriptToGrantReadPermissions
-    Write-Debug "${functionName}:command=$command"
-    
-    [System.IO.FileInfo]$assignReadPermissionsTempFile = [System.IO.Path]::GetTempFileName()
-    [string]$content = Set-Content -Path $assignReadPermissionsTempFile.FullName -Value $command -PassThru -Force
-    Write-Debug "${functionName}:$($assignReadPermissionsTempFile.FullName)=$content"
-    
-    Write-Host "Granting permissions to ${PostgresReaderAdGroup}"
-    $null = Invoke-PSQLScript -PostgresHost $PostgresHost -PostgresDatabase $PostgresDatabase -PostgresUsername $PostgresWriterAdGroup -Path $assignReadPermissionsTempFile.FullName
+    $null = Invoke-PSQLScript -PostgresHost $PostgresHost -PostgresDatabase $PostgresDatabase -PostgresUsername $TeamMIName -Path $assignPermissionsTempFile.FullName
     Write-Host "Granted Access to ${PostgresHost}"
 
     # Successful exit
@@ -122,7 +94,6 @@ catch {
 }
 finally {
     Remove-Item -Path $assignPermissionsTempFile.FullName -Force -ErrorAction SilentlyContinue
-    Remove-Item -Path $assignReadPermissionsTempFile.FullName -Force -ErrorAction SilentlyContinue
 
     [DateTime]$endTime = [DateTime]::UtcNow
     [Timespan]$duration = $endTime.Subtract($startTime)
