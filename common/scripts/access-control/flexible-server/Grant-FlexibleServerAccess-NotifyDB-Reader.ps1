e55fd457159659f2d64beb3be6bbc249d6acfb86
Ken Babigumira <79708209+kbabigumira@users.noreply.github.com>
2024-05-10 16:57:52 +0100

    Script changes to fix Reader permissions

--- common/scripts/access-control/flexible-server/Grant-FlexibleServerAccess-NotifyDB-Reader.ps1
+++ common/scripts/access-control/flexible-server/Grant-FlexibleServerAccess-NotifyDB-Reader.ps1
@@ -48,49 +48,8 @@ Write-Debug "${functionName}:PostgresWriterAdGroup=$PostgresWriterAdGroup"
 [System.IO.DirectoryInfo]$scriptDir = $PSCommandPath | Split-Path -Parent
 Write-Debug "${functionName}:scriptDir.FullName:$($scriptDir.FullName)"
 
-function Get-SQLScriptToCreatePrincipal {
-    [System.Text.StringBuilder]$builder = [System.Text.StringBuilder]::new()
-    [void]$builder.Append(' DO $$ ')
-    [void]$builder.Append(' BEGIN ')
-    [void]$builder.Append("     IF NOT EXISTS (SELECT 1 FROM pgaadauth_list_principals(false) WHERE rolname='$PostgresWriterAdGroup') THEN ")
-    [void]$builder.Append("         RAISE NOTICE 'CREATING PRINCIPAL FOR MANAGED IDENTITY:$PostgresWriterAdGroup';")
-    [void]$builder.Append("         PERFORM pgaadauth_create_principal('$PostgresWriterAdGroup', false, false); ");
-    [void]$builder.Append("         RAISE NOTICE 'PRINCIPAL FOR MANAGED IDENTITY CREATED:$PostgresWriterAdGroup';")
-    [void]$builder.Append('     ELSE ')
-    [void]$builder.Append("         RAISE NOTICE 'PRINCIPAL FOR MANAGED IDENTITY ALREADY EXISTS:$PostgresWriterAdGroup';")
-    [void]$builder.Append('     END IF; ')
-    [void]$builder.Append("     IF NOT EXISTS (SELECT 1 FROM pgaadauth_list_principals(false) WHERE rolname='$PostgresReaderAdGroup') THEN ")
-    [void]$builder.Append("         RAISE NOTICE 'CREATING PRINCIPAL FOR MANAGED IDENTITY:$PostgresReaderAdGroup';")
-    [void]$builder.Append("         PERFORM pgaadauth_create_principal('$PostgresReaderAdGroup', false, false); ");
-    [void]$builder.Append("         RAISE NOTICE 'PRINCIPAL FOR MANAGED IDENTITY CREATED:$PostgresReaderAdGroup';")
-    [void]$builder.Append('     ELSE ')
-    [void]$builder.Append("         RAISE NOTICE 'PRINCIPAL FOR MANAGED IDENTITY ALREADY EXISTS:$PostgresReaderAdGroup';")
-    [void]$builder.Append('     END IF; ')
-    [void]$builder.Append("     EXECUTE ( 'GRANT CONNECT ON DATABASE `"$PostgresDatabase`" TO `"$PostgresWriterAdGroup`"' );")
-    [void]$builder.Append("     EXECUTE ( 'GRANT CONNECT ON DATABASE `"$PostgresDatabase`" TO `"$PostgresReaderAdGroup`"' );")
-    [void]$builder.Append("     RAISE NOTICE 'GRANTED CONNECT TO DATABASE';")
-    [void]$builder.Append(" EXCEPTION ")
-    [void]$builder.Append("     WHEN OTHERS THEN  ")
-    [void]$builder.Append("         RAISE EXCEPTION 'ERROR DURING PRINCIPAL CREATION/GRANT CONNECT: %', SQLERRM; ")
-    [void]$builder.Append(' END $$' )
-    return $builder.ToString()
-}
-
-function Get-SQLScriptToGrantAllPermissions {
-    [System.Text.StringBuilder]$builder = [System.Text.StringBuilder]::new()
-    [void]$builder.Append("GRANT ALL ON SCHEMA public TO `"$PostgresWriterAdGroup`";")
-    [void]$builder.Append("GRANT ALL ON ALL TABLES IN SCHEMA public TO `"$PostgresWriterAdGroup`";")
-    [void]$builder.Append("GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO `"$PostgresWriterAdGroup`";")
-    [void]$builder.Append("GRANT ALL ON ALL FUNCTIONS IN SCHEMA public TO `"$PostgresWriterAdGroup`";")
-    [void]$builder.Append("GRANT ALL ON ALL PROCEDURES IN SCHEMA public TO `"$PostgresWriterAdGroup`";")
-    [void]$builder.Append("ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO `"$PostgresWriterAdGroup`";")
-    [void]$builder.Append("ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO `"$PostgresWriterAdGroup`";")
-    return $builder.ToString()
-}
-
 function Get-SQLScriptToGrantReadPermissions {
     [System.Text.StringBuilder]$builder = [System.Text.StringBuilder]::new()
-    [void]$builder.Append("GRANT USAGE ON SCHEMA public TO `"$PostgresReaderAdGroup`";")
     [void]$builder.Append("GRANT SELECT ON ALL TABLES IN SCHEMA public TO `"$PostgresReaderAdGroup`";")
     [void]$builder.Append("GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO `"$PostgresReaderAdGroup`";")
     [void]$builder.Append("REVOKE EXECUTE ON ALL FUNCTIONS IN SCHEMA public FROM `"$PostgresReaderAdGroup`";")
@@ -101,8 +60,6 @@ function Get-SQLScriptToGrantReadPermissions {
 }
 
 [string]$tempFolder=[System.IO.Path]::GetTempPath()
-[System.IO.FileInfo]$createPrincipalTempFile = $tempFolder + $(New-Guid).ToString() + ".sql";
-[System.IO.FileInfo]$assignAllPermissionsTempFile = $tempFolder + $(New-Guid).ToString() + ".sql";
 [System.IO.FileInfo]$assignReadPermissionsTempFile = $tempFolder + $(New-Guid).ToString() + ".sql";
 
 try {
@@ -119,26 +76,6 @@ try {
     $accessToken = Get-AzAccessToken -ResourceUrl "https://ossrdbms-aad.database.windows.net"
     $ENV:PGPASSWORD = $accessToken.Token
     Write-Host "Access Token Acquired"
-
-    [string]$command = Get-SQLScriptToCreatePrincipal
-    Write-Debug "${functionName}:command=$command"
-    
-    [string]$content = Set-Content -Path $createPrincipalTempFile.FullName -Value $command -PassThru -Force
-    Write-Debug "${functionName}:$($createPrincipalTempFile.FullName)=$content"
-
-    Write-Host "Creating Principal in ${PostgresHost} and Granting connect permissions"
-    $null = Invoke-PSQLScript -PostgresHost $PostgresHost -PostgresDatabase "postgres" -PostgresUsername $PlatformMIName -Path $createPrincipalTempFile.FullName
-    Write-Host "Granted Access to ${PostgresHost}"
-
-    [string]$command = Get-SQLScriptToGrantAllPermissions
-    Write-Debug "${functionName}:command=$command"
-    
-    [string]$content = Set-Content -Path $assignAllPermissionsTempFile.FullName -Value $command -PassThru -Force
-    Write-Debug "${functionName}:$($assignAllPermissionsTempFile.FullName)=$content"
-
-    Write-Host "Granting permissions to ${PostgresWriterAdGroup}"
-    $null = Invoke-PSQLScript -PostgresHost $PostgresHost -PostgresDatabase $PostgresDatabase -PostgresUsername $PlatformMIName -Path $assignAllPermissionsTempFile.FullName
-    Write-Host "Granted Access to ${PostgresWriterAdGroup}"
 
     [string]$command = Get-SQLScriptToGrantReadPermissions
     Write-Debug "${functionName}:command=$command"
@@ -159,8 +96,6 @@ catch {
     throw $_.Exception
 }
 finally {
-    Remove-Item -Path $createPrincipalTempFile.FullName -Force -ErrorAction SilentlyContinue
-    Remove-Item -Path $assignAllPermissionsTempFile.FullName -Force -ErrorAction SilentlyContinue
     Remove-Item -Path $assignReadPermissionsTempFile.FullName -Force -ErrorAction SilentlyContinue
 
     [DateTime]$endTime = [DateTime]::UtcNow
